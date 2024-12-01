e9894893b444956df4a58b5c28fb3976253e519c
HoangQuocDuy29 <quocduyhoang42@gmail.com>
2024-12-01 14:53:38 +0700

    v2

--- hoidanit/src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ hoidanit/src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -3,13 +3,21 @@ package vn.hoidanit.laptopshop;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
 @RestController
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot by Quoc Duy";
+        return "Hello World from Spring Boot byupdate";
     }
-    
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only User ";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only Admin ";
+    }
+
 }
