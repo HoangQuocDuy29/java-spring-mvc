1ce6111f5571477841e1d499d77e95bdf04d6e1b
HoangQuocDuy29 <quocduyhoang42@gmail.com>
2025-01-13 15:42:37 +0700

    017 123 Test Session

--- hoidanit/src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ hoidanit/src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.HttpMessageConverterAuthenticationSuccessHandler.AuthenticationSuccess;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
 import jakarta.servlet.DispatcherType;
 import vn.hoidanit.laptopshop.service.CustomUserDetailsService;
@@ -23,6 +25,16 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() 
         {
             return new BCryptPasswordEncoder();
+        }
+// RememberMe
+    @Bean
+    public SpringSessionRememberMeServices rememberMeServices()
+        {
+            SpringSessionRememberMeServices rememberMeServices =
+                    new SpringSessionRememberMeServices();
+            // optionally customize
+            rememberMeServices.setAlwaysRemember(true);
+            return rememberMeServices;
         }
 // login 
     @Bean
@@ -43,8 +55,9 @@ public class SecurityConfiguration {
             return authProvider;
         }
     
-        @Bean
-        public AuthenticationSuccessHandler customSuccessHandler(){
+    @Bean
+    public AuthenticationSuccessHandler customSuccessHandler()
+        {
             return new CustomSuccessHandler();
         }
         
@@ -62,7 +75,16 @@ public class SecurityConfiguration {
             
             .requestMatchers("/admin/**").hasRole("ADMIN") // chỉ cho admin mới vào đc trang admin
             .anyRequest().authenticated()) //permitAll() là cho tất cả mọi người đi hết,không cần xác thực
-
+            // Luôn tạo session mới,nếu không bị văng ra login
+            .sessionManagement((sessionManagement) -> sessionManagement
+            .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+            .invalidSessionUrl("/logout?expired")
+            //Tại 1 thời điểm có bao nhiêu tk đăng nhập,ở đây là (1)
+            .maximumSessions(1)
+            .maxSessionsPreventsLogin(false))
+            //Mỗi lần đăng xuất sẽ xoá cookie
+            .logout(logout->logout.deleteCookies("JSESSIONID").invalidateHttpSession(true))
+            .rememberMe(r ->r.rememberMeServices(rememberMeServices()))
             .formLogin(formLogin -> formLogin
             .loginPage("/login")
             .failureUrl("/login?error")
@@ -71,4 +93,5 @@ public class SecurityConfiguration {
             .exceptionHandling(ex -> ex.accessDeniedPage("/access-deny"))  ; // 403 
             return http.build();
         }
+        
 }
